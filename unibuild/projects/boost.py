651d8048ec784863713ace8e03a80eb508c7296c
lepresidente <brian.alexander.munro@gmail.com>
2016-12-04 18:43:55 +0200

    Cleaned up code, Fixed missing files that are required by modorganizer.exe.

--- unibuild/projects/boost.py
+++ unibuild/projects/boost.py
@@ -1,8 +1,10 @@
 from unibuild import Project
-from unibuild.modules import b2, sourceforge, patch
+from unibuild.modules import b2, sourceforge, build, patch
 from unibuild.projects import python
 from config import config
 import os
+from glob import glob
+import shutil
 
 
 boost_version = "1.60.0"
@@ -22,30 +24,43 @@ config_template = ("using python : 2.7 : {0}\\PCbuild\\amd64\\python.exe\n"
                    "  : {0}\\Lib\n"
                    "  : <address-model>{1} ;")
 
+
+def install(context):
+    for f in glob(os.path.join(context['build_path'], "stage", "lib", "boost_python-*-mt-*.dll")):
+        shutil.copy(f, os.path.join(config["__build_base_path"], "install", "bin", "dlls"))
+    return True
+
+
 Project("boost") \
-    .depend(b2.B2(name="Shared").arguments(["address-model={}".format("64" if config['architecture'] == 'x86_64' else "32"),
+    .depend(build.Execute(install)
+            .depend(b2.B2(name="Shared")
+                    .arguments(["address-model={}"
+                                .format("64" if config['architecture'] == 'x86_64' else "32"),
                                 "-a",
-                               "-j {}".format(config['num_jobs']),
-                               "toolset=msvc-14.0",
-                               "link=shared",
-                               ] + ["--with-{0}".format(component) for component in boost_components])
-        .depend(b2.B2(name="Static").arguments(["address-model={}".format("64" if config['architecture'] == 'x86_64' else "32"),
-                                "-a",
-                               "-j {}".format(config['num_jobs']),
-                               "toolset=msvc-14.0",
-                               "link=static",
-                               "runtime-link=shared",
-                               ] + ["--with-{0}".format(component) for component in boost_components])
-            .depend(patch.CreateFile("user-config.jam",
-                                     lambda: config_template.format(
-                                             os.path.join(os.path.dirname(python.python['build_path'])),
-                                             "64" if config['architecture'] == "x86_64" else "32")
-                                     )
-                    .depend(sourceforge.Release("boost",
-                                                "boost/{0}/boost_{1}.tar.bz2".format(boost_version,
-                                                                                     boost_version.replace(".", "_")),
-                                                tree_depth=1))
+                                "-j {}".format(config['num_jobs']),
+                                "toolset=msvc-14.0",
+                                "link=shared",
+                                ] + ["--with-{0}".format(component) for component in boost_components])
+                    .depend(b2.B2(name="Static")
+                            .arguments(["address-model={}"
+                                        .format("64" if config['architecture'] == 'x86_64' else "32"),
+                                        "-a",
+                                        "-j {}".format(config['num_jobs']),
+                                        "toolset=msvc-14.0",
+                                        "link=static",
+                                        "runtime-link=shared",
+                                        ] + ["--with-{0}".format(component) for component in boost_components])
+                            .depend(patch.CreateFile("user-config.jam",
+                                                     lambda: config_template.format(
+                                                     os.path.join(os.path.dirname(python.python['build_path'])),
+                                                     "64" if config['architecture'] == "x86_64" else "32")
+                                                    )
+                                    .depend(sourceforge.Release("boost",
+                                                                "boost/{0}/boost_{1}.tar.bz2".format
+                                                                (boost_version,boost_version.replace(".", "_")),
+                                                                tree_depth=1))
+                                    )
+                            )
                     )
+            .depend("Python")
             )
-            ) \
-    .depend("Python")
--- unibuild/projects/sip.py
+++ unibuild/projects/sip.py
@@ -20,17 +20,36 @@ from unibuild import Project
 from unibuild.modules import sourceforge, build
 from subprocess import Popen
 from config import config
+from glob import glob
 import os
 import logging
 import python
+import errno
+import shutil
 
 
 sip_version = "4.16.9"
+
 
 def sip_environment():
     result = config['__environment'].copy()
     result['LIB'] += os.path.join(config['paths']['build'],"Python-2.7.12","PCbuild","amd64")
     return result
+
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+
+def copy_pyd(context):
+        make_sure_path_exists(os.path.join(config["__build_base_path"], "install", "bin", "plugins", "data"))
+        for f in glob(os.path.join(python.python['build_path'], "Lib", "site-packages", "sip.pyd")):
+            shutil.copy(f, os.path.join(config["__build_base_path"], "install", "bin", "plugins", "data"))
+        return True
 
 
 class SipConfigure(build.Builder):
@@ -68,9 +87,14 @@ class SipConfigure(build.Builder):
 
 
 Project('sip') \
-    .depend(build.Make(environment=sip_environment()).install()
-            .depend(SipConfigure()
-                    .depend("Python")
-                    .depend(sourceforge.Release("pyqt", "sip/sip-{0}/sip-{0}.zip".format(sip_version), 1))
+    .depend(build.Execute(copy_pyd)
+            .depend(build.Make(environment=sip_environment()).install()
+                    .depend(SipConfigure()
+                            .depend("Python")
+                                    .depend(sourceforge.Release("pyqt",
+                                                                "sip/sip-{0}/sip-{0}.zip".format(sip_version),
+                                                                1)
+                                            )
+                            )
                     )
             )

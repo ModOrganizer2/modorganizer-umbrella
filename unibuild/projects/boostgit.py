e2fa312a38d1b3545f38151aaef3b55dfef6aeca
Brian Munro <brian.alexander.munro@gmail.com>
2017-03-15 14:06:55 +0200

    Boost needs a few more commands to work when using the git repo.

--- unibuild/projects/boostgit.py
+++ unibuild/projects/boostgit.py
@@ -1,5 +1,5 @@
-from unibuild import Project
-from unibuild.modules import b2, github, patch
+from unibuild import Project, Task
+from unibuild.modules import b2, git, patch, build
 from unibuild.projects import python
 from config import config
 import os
@@ -26,11 +26,15 @@ config_template = ("using python\n"
                    "  : <define>BOOST_ALL_NO_LIB=1\n"
                    "  ;")
 
+init_repo = build.Run("git submodule init && git submodule update", name="init boost repository" ,working_directory=lambda: os.path.join(config["paths"]["build"], "boost_git")) \
+    .set_fail_behaviour(Task.FailBehaviour.CONTINUE) \
+    .depend(git.Clone("https://github.com/boostorg/boost.git", "master").set_destination("boost_git"))
+
 Project("boostgit") \
     .depend(b2.B2(name="Shared").arguments(["address-model={}".format("64" if config['architecture'] == 'x86_64' else "32"),
                                             "-a",
                                             "--user-config={}".format(os.path.join(config['paths']['build'],
-                                                                                   "boost",
+                                                                                   "boost_git",
                                                                                    "user-config.jam")),
                                             "-j {}".format(config['num_jobs']),
                                             "toolset=msvc-15.0",
@@ -43,7 +47,7 @@ Project("boostgit") \
         .depend(b2.B2(name="Static").arguments(["address-model={}".format("64" if config['architecture'] == 'x86_64' else "32"),
                                                 "-a",
                                                 "--user-config={}".format(os.path.join(config['paths']['build'],
-                                                                                       "boost", "user-config.jam")),
+                                                                                       "boost_git", "user-config.jam")),
                                                 "-j {}".format(config['num_jobs']),
                                                 "toolset=msvc-15.0",
                                                 "link=static",
@@ -52,15 +56,18 @@ Project("boostgit") \
                                                 "-sICU_PATH={}".format(os.path.join(config['paths']['build'], "icu", "dist")),
                                                 "-sHAVE_ICU=1",
                                                 ] + ["--with-{0}".format(component) for component in boost_components])
-            .depend(patch.CreateFile("user-config.jam",
+                .depend(build.Run(r"b2.exe headers", working_directory=lambda: os.path.join(config["paths"]["build"], "boost_git"))
+                    .depend(build.Run(r"bootstrap.bat",working_directory=lambda: os.path.join(config["paths"]["build"], "boost_git")))
+                        .depend(patch.CreateFile("user-config.jam",
                                      lambda: config_template.format(
                                          os.path.join(python.python['build_path'], "PCBuild",
                                                       "{}".format("" if config['architecture'] == 'x86' else "amd64")).replace("\\",'/'),
                                         os.path.join(python.python['build_path']).replace("\\",'/'),
                                          "64" if config['architecture'] == "x86_64" else "32")
-                                     )
-                    .depend(github.Source("boostorg", "build", "master").set_destination("boost")
-                    )
+                                     ))
+
+                    .depend(init_repo)
+
         #            .depend(sourceforge.Release("boost",
         #                                        "boost/{0}/boost_{1}.tar.bz2".format(boost_version,
         #                                                                             boost_version.replace(".", "_")),

fcf76244b9e580d09428e7dad5968f86567810e1
Brian Munro <brian.alexander.munro@gmail.com>
2017-03-28 10:55:01 +0200

    NexusModManager now only builds with platform "Any Cpu" msbuild.py added support for custom platform added thirdparty .eggs required to update NexusModManager sln with our custom project

--- unibuild/modules/msbuild.py
+++ unibuild/modules/msbuild.py
@@ -26,11 +26,12 @@ import os
 
 class MSBuild(Builder):
 
-    def __init__(self, solution, project=None, working_directory=None):
+    def __init__(self, solution, project=None, working_directory=None, project_platform=None ):
         super(MSBuild, self).__init__()
         self.__solution = solution
         self.__project = project
         self.__working_directory = working_directory
+        self.__project_platform = project_platform
 
     @property
     def name(self):
@@ -56,8 +57,13 @@ class MSBuild(Builder):
 
         with open(soutpath, "w") as sout:
             with open(serrpath, "w") as serr:
-                args = ["msbuild", self.__solution, "/m", "/property:Configuration=Release", "/property:Platform={}"
-                    .format("x64" if config['architecture'] == 'x86_64' else "win32")]
+                args = ["msbuild", self.__solution, "/m", "/property:Configuration=Release"]
+                if self._context is None:
+                    args.append("/property:Platform={}"
+                        .format("x64" if config['architecture'] == 'x86_64' else "win32"))
+                else:
+                    args.append("/property:Platform={}".format(self.__project_platform))
+
                 if self.__project:
                     args.append("/target:{}".format(self.__project))
 
--- unibuild/projects/ncc.py
+++ unibuild/projects/ncc.py
@@ -15,38 +15,56 @@
 # You should have received a copy of the GNU General Public License
 # along with Mod Organizer.  If not, see <http://www.gnu.org/licenses/>.
 
+import eggs
 from unibuild import Project
 from unibuild.modules import build, msbuild, patch, github
 from unibuild.utility import lazy
-from unimake import get_visual_studio_2017_or_more
 from config import config
 import os
+from buildtools import log
+from buildtools.buildsystem.visualstudio import (ProjectType,
+                                                 VisualStudio2015Solution,
+                                                 VS2015Project)
 
-def upgrade_args():
-    env = config['__environment']
-    devenv_path = env['DevEnvDir'] if 'DevEnvDir' in env\
-        else os.path.join(config['paths']['visual_studio_basedir'], "Common7", "IDE")
-    #MSVC2017 supports building with the MSVC2015 toolset though this will break here, Small work around to make sure devenv.exe exists
-    #If not try MSVC2017 instead
-    res = os.path.isfile(os.path.join(devenv_path, "devenv.exe"))
-    if res:
-        return [os.path.join(devenv_path, "devenv.exe"),
-            "NexusClient.sln",
-            "/upgrade"]
+
+def prepare_nmm(context):
+    sln = VisualStudio2015Solution()
+    sln.LoadFromFile(os.path.join(context['build_path'],'NexusClient.sln'))
+    ncc_csproj = os.path.join(context['build_path'],"..",'NexusClientCLI', 'NexusClientCLI', 'NexusClientCLI.csproj')
+    if not os.path.isfile(ncc_csproj):
+        log.critical('NOT FOUND: %s', ncc_csproj)
     else:
-        return [os.path.join(get_visual_studio_2017_or_more('15.0'),"..","..","..","Common7", "IDE", "devenv.exe"),
-            "NexusClient.sln",
-            "/upgrade"]
+        log.info('FOUND: %s', ncc_csproj)
+    changed = False
+    projfile = VS2015Project()
+    projfile.LoadFromFile(ncc_csproj)
+    projguid = projfile.PropertyGroups[0].element.find('ProjectGuid').text
+    log.info('ProjectGuid = %s', projguid)
+    if "NexusClientCli" not in sln.projectsByName:
+        newproj = sln.AddProject('NexusClientCli', ProjectType.CSHARP_PROJECT, ncc_csproj, guid=projguid)
+        log.info('Adding project %s (%s) to NexusClient.sln', newproj.name, newproj.guid)
+        changed = True
+    else:
+        newproj = sln.projectsByName['NexusClientCli']
+        log.info('Project %s (%s) already exists in NexusClient.sln', newproj.name, newproj.guid)
+        if newproj.projectfile != ncc_csproj:
+            log.info('Changing projectfile: %s -> %s', newproj.projectfile, ncc_csproj)
+            newproj.projectfile = ncc_csproj
+            changed = True
+    if changed:
+        log.info('Writing NexusClientCli.sln')
+        sln.SaveToFile(os.path.relpath(os.path.join(ncc['build_path'],"..","nmm",'NexusClientCli.sln'))) # So we don't get conflicts when pulling.
+        return True
+
 
 ncc = Project("NCC") \
     .depend(build.Run(r"publish.bat"
                      .format("-debug" if config['build_type'] == "Debug" else "-release",
                               os.path.join(config['__build_base_path'], "install", "bin")),
                       working_directory=lazy.Evaluate(lambda: ncc['build_path']))
-#            .depend(msbuild.MSBuild("../nmm/NexusClient.sln",
-#                        working_directory=lazy.Evaluate(lambda: os.path.join(ncc['build_path'], "..", "nmm")))
-#            .depend(build.Run(upgrade_args, name="upgrade ncc project")
-#            .depend(patch.Copy("NexusClient.sln", "../nmm")
+           .depend(msbuild.MSBuild(os.path.join(config['paths']['build'],"NCC","nmm",'NexusClientCli.sln'),
+                       working_directory=lazy.Evaluate(lambda: os.path.join(ncc['build_path'], "..", "nmm")),project_platform="Any CPU")
+            .depend(build.Execute(prepare_nmm, name="append NexusClientCli project to NMM"))
                 .depend(github.Source(config['Main_Author'], "modorganizer-NCC", "master")
                             .set_destination(os.path.join("NCC", "NexusClientCli"))
                             .depend(github.Source("Nexus-Mods", "Nexus-Mod-Manager", "master")
@@ -54,7 +72,5 @@ ncc = Project("NCC") \
                                     )
                             )
                )
-#            )
-#)
-#)
+            )
 

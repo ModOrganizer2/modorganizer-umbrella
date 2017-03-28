0eb56f19e489f075af40d67b57440e6ac9f36afa
lepresidente <brian.alexander.munro@gmail.com>
2017-03-28 18:25:18 +0200

    I was being silly, corrected fetching dep with pip.

--- eggs/__init__.py
+++ eggs/__init__.py
@@ -24,18 +24,12 @@ path = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
 
 for dep in ["https://pypi.python.org/packages/2.7/n/networkx/networkx-1.10-py2.7.egg",
             "https://pypi.python.org/packages/2.5/p/pydot/pydot-1.0.2-py2.5.egg",
-            "https://gitlab.com/LePresidente/python-build-tools/uploads/2f0b7e8340f635002842bb500eb4cb9f/Jinja2-2.9.5-py2.7.egg",
-            "https://gitlab.com/LePresidente/python-build-tools/uploads/18a195f7945ca35ad563b428739f254b/buildtools-0.0.2-py2.7.egg",
-            "https://gitlab.com/LePresidente/python-build-tools/uploads/e6307543843150e8896194b5cd4e2630/MarkupSafe-1.0-py2.7.egg",
-            "https://gitlab.com/LePresidente/python-build-tools/uploads/acf5c7f04e84046f42a812477ed74d76/psutil-5.2.1-py2.7-win-amd64.egg",
-            "https://gitlab.com/LePresidente/python-build-tools/uploads/893dfff5db3aa30fe19e262becce5245/six-1.10.0-py2.7.egg",
-            "https://gitlab.com/LePresidente/python-build-tools/uploads/ad42c9699d5a9e842eeda8ccc291fdda/PyYAML-3.12-py2.7-win-amd64.egg",
-            "https://gitlab.com/LePresidente/python-build-tools/uploads/f280239f8b89713e0b35f038eca151fe/lxml-3.5.0-py2.7-win-amd64.egg"]:
+            "https://gitlab.com/LePresidente/python-build-tools/uploads/18a195f7945ca35ad563b428739f254b/buildtools-0.0.2-py2.7.egg"]:
     eggpath = os.path.join(path, os.path.basename(dep))
     download(dep, eggpath)
     sys.path.append(eggpath)
 
-for dep in ["decorator"]:
+for dep in ["decorator", "lxml", "PyYAML", "six", "jinja2", "psutil"]:
     destpath = "{0}/{1}".format(path, dep)
     if not os.path.exists(destpath):
         pip.main(["install", "--target={0}".format(destpath), dep])

fa75c2fd0ff01b31bea936a7e8a33f5c30c9c80f
LePresidente <brian.alexander.munro@gmail.com>
2017-08-24 12:40:48 +0200

    Fixes https://github.com/LePresidente/modorganizer-umbrella/issues/13

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ config = {
     'Work_Author': 'Hugues92',  			# yourself
 
 	'qt_version':	'5.8',					# currently evolving
-	'openssl_version': '1.0.2k',			# changes often, so better to edit here
+	'openssl_version': '1.0.2L',			# changes often, so better to edit here
 	'zlib_version': '1.2.11',				# changes often, so better to edit here
 	'grep_version': '2.5.4',				# moved here as commented in qt5.py
 	'boost_version': '1.64.0',				# for -DBOOST_ROOT, also, it is either to change from here

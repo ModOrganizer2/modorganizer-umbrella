104b9833b6d2091011f1653a2c488f88cc71028f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2018-03-06 22:24:13 +0100

    Renamed variable for wixtoolset_version

--- unibuild/projects/WixToolkit.py
+++ unibuild/projects/WixToolkit.py
@@ -15,15 +15,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Mod Organizer.  If not, see <http://www.gnu.org/licenses/>.
-
-
 from config import config
 from unibuild import Project
 from unibuild.modules import github
 
-WixToolSet_Version_Binary = config['WixToolSet_Version_Binary']
+WixToolset_version = config['WixToolset_version']
 
 Project("WixToolkit") \
-    .depend(github.Release("wixtoolset", "wix3", "wix{}rtm".format(WixToolSet_Version_Binary),
-                           "wix{}-binaries".format(WixToolSet_Version_Binary))
-            .set_destination("WixToolkit"))
+    .depend(github.Release("wixtoolset", "wix3", "wix{}rtm".format(WixToolset_version), "wix{}-binaries".format(WixToolset_version))
+                  .set_destination("WixToolkit-{}".format(WixToolset_version)))

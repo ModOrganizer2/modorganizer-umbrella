8ad7c4dfac6e3d3493d85cb6ad28d8951fbfa02a
AnyOldName3 <krizdjali+github@gmail.com>
2018-03-11 23:20:42 +0000

    Remove qtdds.dll deployemnt from the fixes project

--- makefile.uni.py
+++ makefile.uni.py
@@ -155,13 +155,8 @@ if config['transifex_Enable']:
 
 def fix(context):
     import shutil
-    try:
-        os.makedirs(os.path.join(config["paths"]["install"], "bin", "dlls", "imageformats"))
-    except:
-        pass
 
     shutil.copy2(os.path.join(qt_inst_path(), "bin", "Qt5WinExtras.dll"), os.path.join(config["paths"]["install"], "bin", "dlls"))
-    shutil.copy2(os.path.join(config['paths']['build'], "modorganizer_super", "modorganizer", "qdds.dll"), os.path.join(config["paths"]["install"], "bin", "dlls", "imageformats"))
     return True
 
 

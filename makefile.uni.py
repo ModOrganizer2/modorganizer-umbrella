f1dc1777e843992b327d465877163474e60a685e
AnyOldName3 <krizdjali+github@gmail.com>
2018-03-12 14:41:56 +0000

    Remove the fixes project entirely as it is redundant.

--- makefile.uni.py
+++ makefile.uni.py
@@ -152,13 +152,3 @@ if config['Installer']:
 if config['transifex_Enable']:
     from unibuild.projects import translations
     translationsBuild = Project("translationsBuild").depend("translations")
-
-def fix(context):
-    import shutil
-
-    shutil.copy2(os.path.join(qt_inst_path(), "bin", "Qt5WinExtras.dll"), os.path.join(config["paths"]["install"], "bin", "dlls"))
-    return True
-
-
-Project("fixes") \
-    .depend(build.Execute(fix).depend("modorganizer"))

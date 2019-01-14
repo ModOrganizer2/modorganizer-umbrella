a7dcb3e4badd54390bc9a957f7a2de1a7cf1e138
Silarn <jrim@rimpo.org>
2019-01-14 13:23:06 -0600

    Update versions of libloot and dev sip/PyQt5

--- config.py
+++ config.py
@@ -91,8 +91,8 @@ config = {
     'grep_version': '2.5.4',
     'icu_version': '63',
     'icu_version_minor': '1',
-    'loot_version': '0.14.0',
-    'loot_commit': 'g2e074a4',
+    'loot_version': '0.14.1',
+    'loot_commit': 'gc8090fe',
     'lz4_version': '1.8.3',
     'lz4_version_minor': '', # leave empty if no patch version (1.2.3.x)
     'nasm_version': '2.13.03',
@@ -100,11 +100,11 @@ config = {
     'nmm_version': '0.65.10',
     'openssl_version': '1.0.2q',
     'pyqt_version': '5.12',
-    'pyqt_dev_version': '1901040954', # leave empty for a standard release
+    'pyqt_dev_version': '1901071751', # leave empty for a standard release
     'python_version': '3.7',
     'python_version_minor': '.1',
     'sip_version': '4.19.14',
-    'sip_dev_version': '1901041429', # leave empty for a standard release
+    'sip_dev_version': '1901071741', # leave empty for a standard release
     'qt_version': '5.12',
     'qt_version_minor': '0',
     'vc_platformtoolset': 'v141',
